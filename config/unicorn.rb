250046a8620de695003e768cf1651e27fe00bfaf
yucha-create <vrc6nes@gmail.com>
2020-09-18 02:34:03 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -15,10 +15,10 @@ pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 
 #エラーのログを記録するファイルを指定
-stderr_path "#{app_path}/shared/tmp/unicorn.stderr.log"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 
 #通常のログを記録するファイルを指定
-stdout_path "#{app_path}/shared/tmp/unicorn.stdout.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
 #Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
